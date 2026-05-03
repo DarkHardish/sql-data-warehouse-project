97d3c801592fef65f77fb09d0c8d29cd1518420e
Jan Pikuła <81435114+DarkHardish@users.noreply.github.com>
2026-05-03 15:59:53 +0200

    Create create_silver_tables.sql

---
+++ scripts/create_silver_tables.sql
@@ -0,0 +1,96 @@
+/*
+===========================================================
+Create Silver Layer Table
+===========================================================
+
+This script recreates all tables in the Silver layer.
+
+Purpose:
+- Defines cleaned and transformed data structures
+- Acts as a staging/processing layer between Bronze and Gold
+- Adds metadata column (dwh_create_date) for tracking load time
+
+WARNING:
+Running this script will DROP existing tables first,
+which means all current data inside these tables
+will be permanently deleted.
+
+Use only in development, testing, or controlled deployment scenarios.
+===========================================================
+*/
+
+
+
+if OBJECT_ID ('silver.crm_cust_info', 'U') is not null
+	drop table silver.crm_cust_info;
+
+create table silver.crm_cust_info (
+	cst_id int,
+	cst_key nvarchar(50),
+	cst_firstname nvarchar(50),
+	cst_lastname nvarchar(50),
+	cst_material_status nvarchar(50),
+	cst_gndr nvarchar(50),
+	cst_create_date date,
+	dwh_create_date datetime2 default getdate()
+);
+
+if OBJECT_ID ('silver.crm_prd_info', 'U') is not null
+	drop table silver.crm_prd_info;
+
+create table silver.crm_prd_info (
+	prd_id int,
+	prd_key nvarchar(50),
+	prd_nm nvarchar(50),
+	prd_cost int,
+	prd_line nvarchar(50),
+	prd_start_dt datetime,
+	prd_end_dt datetime,
+	dwh_create_date datetime2 default getdate()
+);
+
+if OBJECT_ID ('silver.crm_sales_details', 'U') is not null
+	drop table silver.crm_sales_details;
+
+create table silver.crm_sales_details(
+	sls_ord_num nvarchar(50),
+	sls_prd_key nvarchar(50),
+	sls_cust_id int,
+	sls_order_dt int,
+	sls_ship_dt int,
+	sls_due_dt int,
+	sls_sales int,
+	sls_quantity int,
+	sls_price int,
+	dwh_create_date datetime2 default getdate()
+);
+
+if OBJECT_ID ('silver.erp_loc_a101', 'U') is not null
+	drop table silver.erp_loc_a101;
+
+create table silver.erp_loc_a101(
+	cid nvarchar(50),
+	cntry nvarchar(50),
+	dwh_create_date datetime2 default getdate()
+);
+
+if OBJECT_ID ('silver.erp_cust_az12', 'U') is not null
+	drop table silver.erp_cust_az12;
+
+create table silver.erp_cust_az12(
+	cid nvarchar(50),
+	bdate date,
+	gen nvarchar(50),
+	dwh_create_date datetime2 default getdate()
+);
+
+if OBJECT_ID ('silver.erp_px_cat_g1v2', 'U') is not null
+	drop table silver.erp_px_cat_g1v2;
+
+create table silver.erp_px_cat_g1v2(
+	id nvarchar(50),
+	cat nvarchar(50),
+	subcat nvarchar(50),
+	maintenance nvarchar(50),
+	dwh_create_date datetime2 default getdate()
+)
